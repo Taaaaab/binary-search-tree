75fd0dbe17a69fa4f8fe3cae46a04adb312cba16
Taaaaab <owen.whitsett@gmail.com>
2022-09-18 10:03:15 -0400

    Setup script.js file, Node class and Tree class

---
+++ script.js
@@ -0,0 +1,25 @@
+// Build a Node class / factory
+
+class Node {
+    constructor(data, leftChild, rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+}
+
+// Build a Tree class / factory which accepts an array
+
+class Tree {
+    constructor(root) {
+        this.root = root;
+    }
+}
+
+// Write a buildTree function which takes an array 
+// of data and turns it into a balanced binary tree 
+// full of Node objects appropriately placed
+
+function buildTree(array) {
+
+};
