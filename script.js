ac0acdffb694adf35b080550a3885ace9492b9f5
Taaaaab <owen.whitsett@gmail.com>
2022-09-18 15:36:07 -0400

    Merge sort function that removes duplicate numbers from array before building binary search tree

--- script.js
+++ script.js
@@ -11,14 +11,13 @@ class Node {
 // Build a Tree class / factory which accepts an array
 
 class Tree {
-    constructor(root) {
-        this.root = root;
+    constructor(array) {
+        this.array = array;
+        this.root = buildTree(array, 0, n - 1);
     }
 }
 
 let root = null;
-
-
 
 // Write a buildTree function which takes an array 
 // of data and turns it into a balanced binary tree 
@@ -30,50 +29,11 @@ function buildTree(array, start, end) {
         return null;
     }
 
-    const merge = (leftArr, rightArr) => {
-        const output = [];
-        let leftIndex = 0;
-        let rightIndex = 0;
-        
-        while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
-            const leftEl = leftArr[leftIndex];
-            const rightEl = rightArr[rightIndex];
-    
-            if (leftEl < rightEl) {
-                output.push(leftEl);
-                leftIndex++;
-            } else {
-                output.push(rightEl);
-                rightIndex++;
-            }
-        }
-        return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
-    };
-    
-    const mergeSort = arr => {
-        if (arr.length < 2) {
-            return arr;
-        } 
-    
-        // split in half
-        const m = Math.floor(arr.length / 2);
-        const leftArr = arr.slice(0, m);
-        const rightArr = arr.slice(m);
-        
-        // merge sorted sub-arrays
-        return merge(
-            mergeSort(leftArr),
-            mergeSort(rightArr)
-        );
-    };
-
-    let sortedArr = mergeSort(array);
-    
     const mid = parseInt((start + end) / 2);
-    const node = new Node(sortedArr[mid]);
+    const node = new Node(array[mid]);
 
-    node.left = buildTree(sortedArr, start, mid - 1);
-    node.right = buildTree(sortedArr, mid + 1, end);
+    node.left = buildTree(array, start, mid - 1);
+    node.right = buildTree(array, mid + 1, end);
     return node;
 
 };
@@ -88,7 +48,52 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
 }
 
+// Merge Sort to sort array before building tree
+const merge = (leftArr, rightArr) => {
+    const output = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const leftEl = leftArr[leftIndex];
+        const rightEl = rightArr[rightIndex];
+
+        if (leftEl < rightEl) {
+            output.push(leftEl);
+            leftIndex++;
+        } else {
+            output.push(rightEl);
+            rightIndex++;
+        }
+    }
+    return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+};
+
+const mergeSort = arr => {
+    if (arr.length < 2) {
+        return arr;
+    } 
+
+// Remove duplicates from array
+    let uniqueArr = arr.filter((c, index) => {
+    return arr.indexOf(c) === index;
+    });
+
+    // split in half
+    const m = Math.floor(uniqueArr.length / 2);
+    const leftArr = uniqueArr.slice(0, m);
+    const rightArr = uniqueArr.slice(m);
+    
+    // merge sorted sub-arrays
+    return merge(
+        mergeSort(leftArr),
+        mergeSort(rightArr)
+    );
+};
+
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const n = array.length;
-root = buildTree(array, 0, n - 1);
+let sortedArr = mergeSort(array);
+console.log(sortedArr);
+const n = sortedArr.length;
+root = buildTree(sortedArr, 0, n - 1);
 prettyPrint(root);
